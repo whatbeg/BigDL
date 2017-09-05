cfa350171257d1eb2f17c0dd14c795f812604380
huqiu <huqiu00@163.com>
2017-09-05 13:59:35 +0800

    debug mode

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/models/widedeep/Train.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/models/widedeep/Train.scala
@@ -27,7 +27,7 @@ import com.intel.analytics.bigdl.tensor.Tensor
 
 object Train {
   LoggerFilter.redirectSparkInfoLogs()
-  Logger.getLogger("com.intel.analytics.bigdl.optim").setLevel(Level.INFO)
+  Logger.getLogger("com.intel.analytics.bigdl.optim").setLevel(Level.DEBUG)
 
   import Utils._
 
