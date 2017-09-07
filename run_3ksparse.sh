738b86b91d50336e8811d95e911e33ec0d8d8e12
whatbeg <huqiu00@163.com>
2017-09-07 19:56:07 +0800

    runlog

--- run_3ksparse.sh
+++ run_3ksparse.sh
@@ -6,7 +6,7 @@ BigDL_JAR_PATH=${BigDL_HOME}/dist/lib/bigdl-0.2.0-SNAPSHOT-jar-with-dependencies
 PYTHONPATH=${PYTHON_API_PATH}:$PYTHONPATH
 
 # MASTER=spark://172.168.2.160:7077
-MASTER=local[28]
+MASTER=local[24]
 
 PYTHON_API_ZIP_PATH=${BigDL_HOME}/dist/lib/bigdl-0.2.0-SNAPSHOT-python-api.zip
 BigDL_JAR_PATH=${BigDL_HOME}/dist/lib/bigdl-0.2.0-SNAPSHOT-jar-with-dependencies.jar
@@ -16,11 +16,11 @@ logname=`date +%m%d%H%M`
 
 spark-submit \
     --master ${MASTER} \
-    --driver-cores 28 \
-    --driver-memory 180g \
-    --executor-cores 28  \
+    --driver-cores 24 \
+    --driver-memory 20g \
+    --executor-cores 24  \
     --executor-memory 180g \
-    --total-executor-cores 28 \
+    --total-executor-cores 24 \
     --conf spark.rpc.message.maxSize=1024 \
     --properties-file ${BigDL_HOME}/dist/conf/spark-bigdl.conf \
     --jars ${BigDL_JAR_PATH} \
@@ -28,7 +28,7 @@ spark-submit \
     --conf spark.executor.extraClassPath=bigdl-0.2.0-SNAPSHOT-jar-with-dependencies.jar \
     --class com.intel.analytics.bigdl.models.widedeep_tutorial.Train \
     ${BigDL_JAR_PATH} \
-    -f hdfs://172.168.2.160:9000/census \
-    -b 1344 \
+    -f ${BigDL_HOME}/census \
+    -b 1152 \
     -e 100 \
-    -r 0.001 |& tee LOG/BigDL_3k_sparse_1280_local20_${logname}.log
+    -r 0.001 |& tee LOG/BigDL_3k_sparse_1152_local24_${logname}.log
