b9bf7a171823a7ed2ff9104112c4251cc40aabc5
whatbeg <huqiu00@163.com>
2017-09-20 14:18:41 +0800

    fix SparseTensorBLAS

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/tensor/SparseTensorMathSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/tensor/SparseTensorMathSpec.scala
@@ -28,11 +28,47 @@ class SparseTensorMathSpec extends FlatSpec with Matchers {
     val a = Tensor[Float](3)
     a.setValue(1, 2)
     a.setValue(3, 1)
-    var res = Tensor[Float](2)
+    val res = Tensor[Float](2)
     SparseTensorMath.addmv[Float](res, 1, Tensor[Float](2).fill(0), 1, sparseM, a)
     val correctRes = Tensor[Float](2)
     correctRes.setValue(1, 7)
     correctRes.setValue(2, 25)
+
+    res shouldEqual correctRes
+  }
+
+  "Sparse Matrix * Dense Matrix" should "be correct" in {
+    val sparseMatrix: Tensor[Float] = new DenseTensor[Float](2, 3)
+    sparseMatrix.setValue(1, 3, 1)
+    sparseMatrix.setValue(2, 2, 1)
+    val sparseM = Tensor.sparse(sparseMatrix)
+    val denseM = Tensor[Float](2, 3).range(1, 12, 2).t()
+
+    val res = Tensor[Float](2, 2)
+    SparseTensorMath.addmm[Float](res, 1, res, 1, sparseM, denseM)
+    val correctRes = Tensor[Float](2, 2)
+    correctRes.setValue(1, 1, 5)
+    correctRes.setValue(1, 2, 11)
+    correctRes.setValue(2, 1, 3)
+    correctRes.setValue(2, 2, 9)
+
+    res shouldEqual correctRes
+  }
+
+  "Dense Matrix * Sparse Matrix" should "be correct" in {
+    val sparseMatrix: Tensor[Float] = new DenseTensor[Float](3, 2)
+    sparseMatrix.setValue(2, 2, 1)
+    sparseMatrix.setValue(3, 1, 1)
+    val sparseM = Tensor.sparse(sparseMatrix)
+    val denseM = Tensor[Float](2, 3).range(1, 12, 2)
+
+    val res = Tensor[Float](2, 2)
+    SparseTensorMath.addmm[Float](res, 1, res, 1, denseM, sparseM)
+    val correctRes = Tensor[Float](2, 2)
+    correctRes.setValue(1, 1, 5)
+    correctRes.setValue(1, 2, 3)
+    correctRes.setValue(2, 1, 11)
+    correctRes.setValue(2, 2, 9)
 
     res shouldEqual correctRes
   }
